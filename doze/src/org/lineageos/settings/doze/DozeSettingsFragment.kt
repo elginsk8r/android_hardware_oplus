e5e15330ef6474513791961b9c551aef1d711f47
Nick Reuter <nreuter85@gmail.com>
2021-11-02 00:12:35 -0500

    doze: update app for AOSP flow
     Use IA settings
     Remove preferences already defined in AOSP Settings
    
    Change-Id: I976a56c813d790828ca31809b82335c7d46dec94

--- doze/src/org/lineageos/settings/doze/DozeSettingsFragment.kt
+++ doze/src/org/lineageos/settings/doze/DozeSettingsFragment.kt
@@ -5,21 +5,12 @@
 
 package org.lineageos.settings.doze
 
-import android.app.Activity
-import android.app.AlertDialog
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.widget.Switch
 import androidx.preference.*
 
-import com.android.settingslib.widget.MainSwitchPreference
-import com.android.settingslib.widget.OnMainSwitchChangeListener
-
-class DozeSettingsFragment : PreferenceFragment(), Preference.OnPreferenceChangeListener,
-    OnMainSwitchChangeListener {
-    private lateinit var alwaysOnDisplayPreference: SwitchPreference
-    private lateinit var switchBar: MainSwitchPreference
+class DozeSettingsFragment : PreferenceFragment(), Preference.OnPreferenceChangeListener {
 
     private var pickUpPreference: ListPreference? = null
     private var pocketPreference: SwitchPreference? = null
@@ -29,26 +20,7 @@ class DozeSettingsFragment : PreferenceFragment(), Preference.OnPreferenceChange
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.doze_settings)
 
-        val prefs = activity.getSharedPreferences("doze_settings", Activity.MODE_PRIVATE)!!
-        if (savedInstanceState == null && !prefs.getBoolean("first_help_shown", false)) {
-            AlertDialog.Builder(context)
-                .setTitle(R.string.doze_settings_help_title)
-                .setMessage(R.string.doze_settings_help_text)
-                .setNegativeButton(R.string.dialog_ok) { _, _ ->
-                    prefs.edit().putBoolean("first_help_shown", true).apply()
-                }
-                .show()
-        }
-
         val dozeEnabled = Utils.isDozeEnabled(context)
-        switchBar = findPreference(Utils.DOZE_ENABLE)!!
-        switchBar.addOnSwitchChangeListener(this)
-        switchBar.isChecked = dozeEnabled
-
-        alwaysOnDisplayPreference = findPreference(Utils.ALWAYS_ON_DISPLAY)!!
-        alwaysOnDisplayPreference.isEnabled = dozeEnabled
-        alwaysOnDisplayPreference.isChecked = Utils.isAlwaysOnEnabled(context)
-        alwaysOnDisplayPreference.onPreferenceChangeListener = this
 
         val pickupSensorCategory =
             preferenceScreen.findPreference<PreferenceCategory>(Utils.CATEGORY_PICKUP_SENSOR)
@@ -63,44 +35,28 @@ class DozeSettingsFragment : PreferenceFragment(), Preference.OnPreferenceChange
         }
 
         pickUpPreference = findPreference(Utils.GESTURE_PICK_UP_KEY)
-        pickUpPreference?.isEnabled = dozeEnabled
         pickUpPreference?.onPreferenceChangeListener = this
 
+        // Hide AOD if not supported and set all its dependents otherwise
+        if (Utils.alwaysOnDisplayAvailable(context)) {
+            pickUpPreference?.isEnabled = !Utils.isAlwaysOnEnabled(context) && dozeEnabled
+        } else {
+            pickUpPreference?.isEnabled = dozeEnabled
+        }
+
         pocketPreference = findPreference(Utils.GESTURE_POCKET_KEY)
-        pocketPreference?.isEnabled = dozeEnabled
         pocketPreference?.onPreferenceChangeListener = this
 
         // Hide AOD if not supported and set all its dependents otherwise
-        if (!Utils.alwaysOnDisplayAvailable(context)) {
-            preferenceScreen.removePreference(alwaysOnDisplayPreference)
+        if (Utils.alwaysOnDisplayAvailable(context)) {
+            pocketPreference?.isEnabled = !Utils.isAlwaysOnEnabled(context) && dozeEnabled
         } else {
-            pickupSensorCategory?.dependency = Utils.ALWAYS_ON_DISPLAY
-            proximitySensorCategory?.dependency = Utils.ALWAYS_ON_DISPLAY
+            pocketPreference?.isEnabled = dozeEnabled
         }
     }
 
     override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
-        if (preference.key == Utils.ALWAYS_ON_DISPLAY) {
-            Utils.enableAlwaysOn(context, newValue as Boolean)
-        }
         handler.post { Utils.checkDozeService(context) }
         return true
     }
-
-    override fun onSwitchChanged(switchView: Switch, isChecked: Boolean) {
-        Utils.enableDoze(context, isChecked)
-        Utils.checkDozeService(context)
-
-        switchBar.isChecked = isChecked
-
-        if (!isChecked) {
-            Utils.enableAlwaysOn(context, false)
-            alwaysOnDisplayPreference.isChecked = false
-        }
-
-        alwaysOnDisplayPreference.isEnabled = isChecked
-        pickUpPreference?.isEnabled = isChecked
-        pocketPreference?.isEnabled = isChecked
-    }
-
 }
